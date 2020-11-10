3779b373ddb36329c02ccd7afd39107de5070454
higorsantana-omega <omegadosgames02@gmail.com>
2020-11-10 14:01:40 -0300

    sequence

--- main.py
+++ main.py
@@ -2,3 +2,18 @@ import secrets
 import string
 import random
 
+class Application(): 
+    # sequence
+    @staticmethod
+    def sequence(conditions):
+        possible_characters = [string.ascii_lowercase, string.ascii_uppercase, string.digits, string.punctuation]
+        sequence = ''
+        for x in range(len(conditions)):
+            if conditions[x]:
+                sequence += possible_characters[x]
+            else:
+                pass
+        return sequence
+
+
+    # password
