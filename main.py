c6f7bb16b2bb69a078078122fc46ccdc993ddd53
higorsantana-omega <omegadosgames02@gmail.com>
2020-11-11 15:37:23 -0300

    reconfigured

--- main.py
+++ main.py
@@ -2,25 +2,11 @@ import secrets
 import string
 import random
 
-class Application(): 
-    # sequence
-    @staticmethod
-    def sequence(conditions):
-        possible_characters = [string.ascii_lowercase, string.ascii_uppercase, string.digits, string.punctuation]
-        sequence = ''
-        for x in range(len(conditions)):
-            if conditions[x]:
-                sequence += possible_characters[x]
-            else:
-                pass
-        return sequence
 
-    @staticmethod
-    def p_password(sequence, passlength=8):
-        password = ''.join(secrets.choice(sequence) for i in range(passlength))
-        return password
+characters = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+password = []
 
-Application()
+for x in range(8):
+    password.append(random.choice(characters))
 
-
-    # password
+print(''.join(password))
