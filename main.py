ea4044c2ed0f7925d7c13ef954fc721859913435
higorsantana-omega <omegadosgames02@gmail.com>
2020-11-11 15:45:48 -0300

    window simple

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import secrets
 import string
 import random
+from tkinter import *
+
+root = Tk()
+
+class Application():
+    def __init__(self):
+        self.root = root
+        self.root.mainloop()
+
 
 characters = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
 password = []
@@ -9,3 +18,5 @@ for x in range(8):
     password.append(secrets.choice(characters))
 
 print(''.join(password))
+
+Application()
