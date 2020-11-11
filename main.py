85fb5ea3248c691cf1aa0b7320112676501b9821
higorsantana-omega <omegadosgames02@gmail.com>
2020-11-11 15:54:06 -0300

    frame

--- main.py
+++ main.py
@@ -8,7 +8,24 @@ root = Tk()
 class Application():
     def __init__(self):
         self.root = root
+        self.window()
+        self.frame()
+        self.labels()
         self.root.mainloop()
+
+    def window(self):
+        self.root.geometry('300x300')
+        self.root.title('Password Generator')
+
+    def frame(self):
+        self.password_fr = Frame(self.root, width=300, height=150, bg='blue')
+        self.password_fr.grid(row=0, column=0)
+
+        self.password_bt = Frame(self.root, width=300, height=150, bg='red')
+        self.password_bt.grid(row=1, column=0)
+
+    def labels(self):
+        pass
 
 
 characters = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
