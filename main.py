9aef9e7f5436c9b1c4568b9ef34696c55ecd135e
higorsantana-omega <omegadosgames02@gmail.com>
2020-11-12 17:05:30 -0300

    espaços apagados e novo modo de criar senha

--- main.py
+++ main.py
@@ -15,33 +15,14 @@ class Application():
         self.password_entry = Entry(self.root, width=30)
         self.password_entry.grid(row=0, column=0)
         
-<<<<<<< HEAD
         self.password_lb = Button(self.root, text='Gen', command=self.gen_password)
         self.password_lb.grid(row=0, column=1)
 
     def gen_password(self):
-        self.characters = [string.ascii_lowercase, string.ascii_uppercase, string.digits, string.punctuation]
-        self.sequence=
-        self.password = ''
-
-        for x in range(8):
-            self.password.join(secrets.choice(self.characters))
-            return self.password
-
+        self.characters = string.ascii_lowercase + string.ascii_uppercase + string.digits
+        self.password = ''.join(secrets.choice(self.characters) for i in range(8))
         self.password_entry.delete(0, END)
         print(self.password)
         self.password_entry.insert(0, self.password)
-=======
-        self.password_lb = Button(self.root, text='Gen')
-        self.password_lb.pack()
-
-characters = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
-password = []
-
-for x in range(8):
-    password.append(secrets.choice(characters))
-
-print(''.join(password))
->>>>>>> parent of abc4756... entry with pass
 
 Application()
