f9b94a11c8020503ce95394558f49734f2b4e96b
higorsantana-omega <omegadosgames02@gmail.com>
2020-11-12 17:35:44 -0300

    combobox

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import secrets
 import string
 import random
 from tkinter import *
+from tkinter import ttk
 
 root = Tk()
 
@@ -9,7 +10,6 @@ class Application():
     def __init__(self):
         self.root = root
         self.labels()
-        self.test_secure()
         self.root.mainloop()
 
     def labels(self):
@@ -17,7 +17,10 @@ class Application():
         self.password_entry.grid(row=0, column=0)
         
         self.password_lb = Button(self.root, text='Gen', command=self.gen_password)
-        self.password_lb.grid(row=0, column=1)
+        self.password_lb.grid(row=0, column=2)
+
+        self.password_combo = ttk.Combobox(self.root, width=3, font='Arial 12 bold')
+        self.password_combo.grid(row=0, column=1)
 
     def gen_password(self):
         self.characters = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
@@ -25,8 +28,5 @@ class Application():
         self.password_entry.delete(0, END)
         print(self.password)
         self.password_entry.insert(0, self.password)
-
-    def test_secure(self):
-        pass
 
 Application()
