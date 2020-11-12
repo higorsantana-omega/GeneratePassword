985619c0c260638acd663d62d27e8557e1b95d4f
higorsantana-omega <omegadosgames02@gmail.com>
2020-11-12 16:52:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -13,18 +13,22 @@ class Application():
 
     def labels(self):
         self.password_entry = Entry(self.root, width=30)
-        self.password_entry.pack()
+        self.password_entry.grid(row=0, column=0)
         
-        self.password_lb = Button(self.root, text='Gen', command=self.gen_password())
-        self.password_lb.pack()
+        self.password_lb = Button(self.root, text='Gen', command=self.gen_password)
+        self.password_lb.grid(row=0, column=1)
 
     def gen_password(self):
-        self.characters = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
-        self.password = []
+        self.characters = [string.ascii_lowercase, string.ascii_uppercase, string.digits, string.punctuation]
+        self.sequence=
+        self.password = ''
 
         for x in range(8):
-            self.password.append(secrets.choice(self.characters))
+            self.password.join(secrets.choice(self.characters))
+            return self.password
 
+        self.password_entry.delete(0, END)
+        print(self.password)
         self.password_entry.insert(0, self.password)
 
 Application()
